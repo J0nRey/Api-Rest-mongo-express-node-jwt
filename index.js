013b7f098777803b0d0d6068d4be4bd873acabd8
J0nRey <79552541+J0nRey@users.noreply.github.com>
2024-01-24 20:54:01 -0600

    intento de deploy

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ const whiteList = [ process.env.ORIGIN1, process.env.ORIGIN2 ]
 app.use(
     cors({
         origin: function(origin, callback){
-            if(whiteList.includes(origin)){
+            console.log("origin => ", origin)
+            if(!origin || whiteList.includes(origin)){
                 return callback(null, origin)
             }
             return callback(
